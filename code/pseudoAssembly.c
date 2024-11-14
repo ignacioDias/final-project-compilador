47f64ea4c3c86af14ce3f61592d3e25f26dc3d2c
ignacioDias <ignadiasg@gmail.com>
2024-11-14 01:54:16 -0300

    avances..(?

--- code/pseudoAssembly.c
+++ code/pseudoAssembly.c
@@ -3,6 +3,7 @@
 #include "../libraries/pseudoAssembly.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 AssemblyList *program;
 
@@ -32,6 +33,67 @@ void insertNode(Triple *triple) {
         newNode->next = newNode;
         program = newNode;
     }
+}
+
+void convert(Operation op, Tree* value1, Tree* value2, Tree* temporary) {
+    Triple* value1Node = value1->info;
+    Triple* value2Node;
+    Triple* resSymbol = temporary->info;
+    Triple* tripleValue2;
+
+    if(value2) {
+        value2Node = value2->info;
+        tripleValue2 = getVar(value2->symbol->varname);
+    }
+
+    switch(op) {
+        case ASIGN:
+            if(tripleValue2){
+                value1Node->value = tripleValue2->value;
+                resSymbol = value1Node;
+            } else {
+                value1Node->value = value2Node->value;
+                resSymbol = value1Node;
+            }
+            createInstallSequence(T_ASIGN, value1Node, value2Node, resSymbol);
+            break;
+        case MENOR:
+        case MAYOR:
+        case IGUAL:
+        case MENOS:
+        case SUMA:
+        case MULT:
+        case DIV:
+        case MOD:
+        case OR:
+        case AND:
+        case NEG:
+        case IFF:
+            handleBinaryOperation(tag, value1Node, value2Node, resSymbol);
+            break;
+        case EWHILE:
+            if (value1Node->value == 1)
+                resSymbol = value2->right->symbol;
+            else
+                resSymbol = NULL;
+            handleBinaryOperation(T_WHILE, value1Node, value2Node, resSymbol);
+            break;
+    }
+}
+
+Triple *getVar(char* name) {
+    if(!name || !program) {
+        perror("Error: getVar");
+        exit(1);
+    }
+    AssemblyList *current = program;
+    while(current) {
+        if(strcmp(current->info->temporary->name, name) == 0) {
+            return current->info;
+        }
+        current = current->next;
+    }
+    return NULL;
 }
 
 
