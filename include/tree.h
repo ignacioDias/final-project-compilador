fa7ea1dfa0555e9aecf77633a3c1e588605e3ce4
Ignacio Dias Gundin <ignadiasg@gmail.com>
2025-08-31 16:30:39 -0300

    cambios en tdata y tokens

--- include/tree.h
+++ include/tree.h
@@ -3,13 +3,13 @@
 #include <string.h>
 #include <stdbool.h>
 #include "symbols_table.h"
-#ifndef AST_H
-#define AST_H
+#ifndef TREE_H
+#define TREE_H
 
-typedef struct AST {
+typedef struct BinaryTree {
     struct TData *symbol;
-    struct AST *left;
-    struct AST *right;
+    struct BinaryTree *left;
+    struct BinaryTree *right;
 } Tree;
 
 Tree* createTree(TData* symbol, Tree *l, Tree *r);
@@ -19,8 +19,6 @@ void printDot(Tree* tree, const char* filename);
 void elimArbol(Tree* tree);
 
 void createTable(Tree* tree);
-void typeError(Tree* tree);
-void evaluate(Tree* tree);
 
 bool getError();
 void retError();
